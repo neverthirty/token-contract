9438f7f0565c86b2320350515f67b39880577688
Arseny271 <arsendmitr@gmail.com>
2022-09-04 20:40:30 +0400

    Update code

--- nft/compile.sh
+++ nft/compile.sh
@@ -2,6 +2,6 @@ rm build/nft-item-editable-code.fif
 rm build/nft-collection-editable-code.fif
 
 func -o build/nft-item-editable-code.fif -SPA stdlib.fc params.fc op-codes.fc nft-item-editable-DRAFT.fc
-func -o build/nft-collection-editable-code.fif -SPA stdlib.fc params.fc op-codes.fc nft-collection-editable.fc
+func -o build/nft-collection-editable-code.fif -SPA stdlib.fc params.fc op-codes.fc nft-collection-utils.fc nft-collection-editable.fc
 
 fift -s build/print-hex.fif
